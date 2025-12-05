4d7c6952f4616f8d0aea2a8ab9289038794ebf62
Yash Gupta <yashgupta@spinfluencemedia.com>
2025-12-05 14:48:33 +0530

    Commented faculty grid

--- src/pages/Faculty.jsx
+++ src/pages/Faculty.jsx
@@ -125,7 +125,7 @@ const Faculty = () => {
       </section>
 
       {/* Faculty Members Grid */}
-      <section className="py-16 bg-gray-50">
+      {/* <section className="py-16 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 text-center">
             Meet Our Team
@@ -182,7 +182,7 @@ const Faculty = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Faculty Qualifications */}
       <section className="py-16 bg-white">
